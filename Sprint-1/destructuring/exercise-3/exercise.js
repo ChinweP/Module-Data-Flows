253c6fcba56b080f74fb5afd3df8d1727b8784fa
ChinweP <exponentialresults1973@gmail.com>
2026-08-01 01:12:08 +0100

    Generate receipt by destructuring order items and calculating totals

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,21 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+
+console.log("QTY     ITEM                TOTAL");
+
+let total = 0;
+
+order.forEach((product) => {
+  const { itemName, quantity, unitPricePence } = product;
+
+  const price = unitPricePence / 100;
+  const lineTotal = quantity * price;
+  total += lineTotal;
+
+  console.log(
+    `${quantity}       ${itemName.padEnd(18)} ${lineTotal.toFixed(2)}`
+  );
+});
+
+console.log(`\nTotal: ${total.toFixed(2)}`);
